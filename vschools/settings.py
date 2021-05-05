716e910acc826e2cf8ada9aca719eb944d608420
Katumba Victor <katumbavictor5@gmail.com>
2021-05-05 04:17:22 -0700

    another

--- vschools/settings.py
+++ vschools/settings.py
@@ -124,9 +124,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static files')]
+STATIC_ROOT = os.path.join(BASE_DIR,'static files')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
